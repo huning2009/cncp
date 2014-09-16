9c2aca520a8f88798c1ed6f1b254530f170ec34d
Simon Dobson <simon.dobson@computer.org>
2014-09-16 15:43:19 +0100

    Fixed some metadata

--- vm/provisioning/cookbooks/apt-get-update/metadata.rb
+++ vm/provisioning/cookbooks/apt-get-update/metadata.rb
@@ -1,7 +1,7 @@
 name             'apt-get-update'
 maintainer       'Simon Dobson'
 maintainer_email 'simon.dobson@computer.org'
-license          'All rights reserved'
+license          'GPL v3'
 description      'Runs apt-get update'
 long_description 'Updates the APT cache ahead of provisioning'
 version          '0.1.0'
