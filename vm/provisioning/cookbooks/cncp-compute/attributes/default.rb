23d5cafafdd74132857eb00f67b5cfb5e50992ee
Simon Dobson <simon.dobson@computer.org>
2015-07-10 16:51:36 +0100

    Removed attributes

---
+++ vm/provisioning/cookbooks/cncp-compute/attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: cncp-worker
+#
+# Copyright (C) 2015 Simon Dobson
+# 
+# Licensed under the Creative Commons Attribution-Noncommercial-Share
+# Alike 3.0 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+#
+
+
